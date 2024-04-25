bae5b60472133b3d46574e2607c72519fee0f9d0
Xin <chengxinjun759@gmail.com>
2024-04-25 11:52:11 +0800

    BMI Code

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,16 @@
-def calculate_bmi
+def calculate_bmi(h,w):
+    print("Height = "+ str(h))
+    print("Weight = "+ str(w))
+    H=h*h
+    BMI=w/H
+    print("Your BMI =" + str(BMI))
+    if BMI<18.5:
+        print ("Skinny")
+    elif 18.5<=BMI<=25.0:
+        print("Normal")
+    elif BMI > 25.0:
+        print("Fatt")
+
+
+
+calculate_bmi(w=69,h=1.73) 
