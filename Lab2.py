0481ed88e6d37c330edb4ceb2ab825ddff33d078
Xin <chengxinjun759@gmail.com>
2024-05-02 10:59:11 +0800

    added test_bmi.py

--- Lab2.py
+++ Lab2.py
@@ -1,17 +1,20 @@
-def calculate_bmi(h,w):
+def calculate_bmi(w,h):
     print("Height = "+ str(h))
     print("Weight = "+ str(w))
-    H=h*h
-    BMI=w/H
+    
+    H = h * h
+    BMI = w / H
+
+
     print("Your BMI =" + str(BMI))
     if BMI<18.5:
         print ("Skinny")
         return -1
-    elif 18.5<=BMI<=25.0:
+    elif 18.5<=BMI<=25.0: 
         print("Normal")
         return 0
     elif BMI > 25.0:
-        print("Fatt")
+        print("Fat")
         return 1
 
 
